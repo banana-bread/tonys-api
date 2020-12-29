52d78dd1c4d68bbd5a691ab03b62870342dbd084
Adriano Dramisino <dramisinoadriano@gmail.com>
2020-12-28 23:12:25 -0500

    config api routes

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/', function(Request $r) {
+    return response()->json('tony\'s api');
 });
