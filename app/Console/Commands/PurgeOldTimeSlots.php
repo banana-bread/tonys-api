32f53ddc3a1db63da7dc56a41dc2d978f30c6453
adriano <dramisinoadriano@gmail.com>
2022-03-30 19:39:35 -0400

    Added job for purging old time slots

---
+++ app/Console/Commands/PurgeOldTimeSlots.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\TimeSlot;
+use Illuminate\Console\Command;
+
+class PurgeOldTimeSlots extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'timeslots:purge';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Purge passed time slots.';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        TimeSlot::where('end_time', '<', now()->subDay())->delete();
+
+        return Command::SUCCESS;
+    }
+}
