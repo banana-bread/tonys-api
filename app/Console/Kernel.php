1a72a6fb980509654027c0c46c068481ff7d853c
adriano <dramisinoadriano@gmail.com>
2022-08-04 09:05:15 -0400

    added logic for schdeulding time slot creation

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\Models\Employee;
+use App\Models\TimeSlot;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -24,9 +26,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('slots:purge')
-            ->monthlyOn(1, '03:00')
-            ->environments(['production']);
+        $this->purgeOldSlots($schedule);
+        $this->createNewSlots($schedule);
     }
 
     /**
@@ -39,5 +40,39 @@ class Kernel extends ConsoleKernel
         $this->load(__DIR__.'/Commands');
 
         require base_path('routes/console.php');
+    }
+
+    private function purgeOldSlots(Schedule $schedule)
+    {
+      $schedule->command('slots:purge')
+            ->monthlyOn(1, '03:00')
+            ->environments(['production']);
+    }
+
+    private function createNewSlots(Schedule $schedule)
+    {
+      // 1. schedule jobs monthly
+      $schedule->call(function() 
+      {
+        // 2. get employee ids that we should create future time slots for
+        $employeeIdsToCreateSlotsFor = 
+          TimeSlot::whereNotExists(function($query) {
+            $query->select('employee_id')
+              ->from('time_slots')
+              ->where('start_time', '>', now()->addDays(90));
+          })
+          ->groupBy('employee_id')
+          ->pluck('employee_id');
+        
+        // 3. create batches of create time slot jobs, 5 employees per job.
+        $createNewSlotsJobs = collect($employeeIdsToCreateSlotsFor)->chunk(5)->map(
+          fn($employeeIds) => new CreateTimeSlots($employeeIds)
+        );
+
+        // 4. dispatch job bus
+        Bus::Batch($createNewSlotsJobs)->dispatch();
+      })
+      ->monthlyOn(2, '03:00')
+      ->environments(['production']);
     }
 }
